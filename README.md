ee4ea6e0e6053904165bc91f671ebd2aed41da30
MOgorodnik <mr.ogorodnik@gmail.com>
2022-02-12 20:24:08 +0200

    Mocking GET routes...

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # LinuxFoundationX_LFW111x
+
+## Mocking GET Routes (1 - 6)
+
+### Actions for install needed modules after branch was pulling
+
+!!! **mock-srv** should be our current working directory
+
+If you on the root directory do **Step 1**.
+
+If you on `mock-srv` already do **Step 2**.
+```
+// Step 1 
+cd mock-srv
+
+// Step 2
+npm init fastify
+```
+
+- Install dependencies
+- Setup CORS access
+- Create our two GET routes
+
+```
+// Step 3
+npm install
+
+// Step 4
+npm install fastify-cors
+```
+
+At this Chapter was modified `mock-srv/app.js`
+```
+const path = require('path')
+const AutoLoad = require('fastify-autoload')
++++ const cors = require('fastify-cors')
+
+module.exports = async function (fastify, opts) {
++++  fastify.register(cors)
+
+  fastify.register(AutoLoad, {
+      ...
+```
+Was created route's folders inside our current working directory `mock-srv/routes`
+- ***confectionery***
+- ***electronics***
+
+Was created the `index.js` files inside both of folders from step above:
+- `mock-srv/routes/confectionery/index.js`
+- `mock-srv/routes/electronics/index.js`
+
+---
+
+## Konsole command for run App && Server
+
+**App** --> **root** directory
+```
+serve static -p 3003
+```
+**Server**  -->  **mock-srv** directory
+```
+npm run dev
+```
--- static/app.js
+++ static/app.js
@@ -1,32 +1,50 @@
-const API = 'http://localhost:3001'
+const API = 'http://localhost:3000'
 
-const populateProducts = async () => {
-    const products = document.querySelector('#products')
-    products.innerHTML = ''
-    // The inline array assigned to the data constant is now replaced with:
-    const res = await fetch(API)
-    const data = await res.json()
-    // **
-    for (const product of data) {
-      const item = document.createElement('product-item')
-      for (const key of ['name', 'rrp', 'info']) {
-        const span = document.createElement('span')
-        span.slot = key
-        span.textContent = product[key]
-        item.appendChild(span)
-      }
-      products.appendChild(item)
+// start
+// --
+// const populateProducts = async () => {
+// ++
+const populateProducts = async (categoryName) => {
+// end
+  const products = document.querySelector('#products')
+  products.innerHTML = ''
+  // start
+  // --
+  // const res = await fetch(API)
+  // ++
+  const res = await fetch(`${API}/${categoryName}`)
+  // end
+  const data = await res.json()
+  // **
+  for (const product of data) {
+    const item = document.createElement('product-item')
+    for (const key of ['name', 'rrp', 'info']) {
+      const span = document.createElement('span')
+      span.slot = key
+      span.textContent = product[key]
+      item.appendChild(span)
     }
+    products.appendChild(item)
   }
+}
   
-  document.querySelector('#fetch').addEventListener('click', async () => {
-    await populateProducts()
-  })
-  
-  customElements.define('product-item', class Item extends HTMLElement {
-    constructor() {
-      super()
-      const itemTmpl = document.querySelector('#item').content
-      this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true))
-    }
-  })
+// start
+// --
+// document.querySelector('#fetch').addEventListener('click', async () => {
+//   await populateProducts()
+// })
+// ++
+const category = document.querySelector('#category')
+// ++
+category.addEventListener('input', async ({ target }) => {
+  await populateProducts(target.value)
+})
+// end
+
+customElements.define('product-item', class Item extends HTMLElement {
+  constructor() {
+    super()
+    const itemTmpl = document.querySelector('#item').content
+    this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true))
+  }
+})
